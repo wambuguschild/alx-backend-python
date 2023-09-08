d285f1da1ad83b9189109f8d4ff7394a880339d8
Jedidah <jedidahwambuguw@gmail.com>
2023-09-08 12:08:54 +0300

    Fix errors

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -9,5 +9,4 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """ async generator"""
-    results = [i async for i in async_generator()]
-    return results
+    return [i async for i in async_generator()]
