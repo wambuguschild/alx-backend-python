81b2445292e0722b697bfa42ab055bf7eda9c4de
Jedidah <jedidahwambuguw@gmail.com>
2023-10-26 21:42:53 +0300

    Fixing errors

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-write an async comprehension
+An async comprehension script
 """
 
 import asyncio
@@ -11,5 +11,13 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> List[float]:
-    """ async generator"""
+    """ Collect 10 random numbers using async comprehension"""
     return [i async for i in async_generator()]
+
+
+if __name__ == '__main__':
+    async def main():
+        result = await async_comprehension()
+        print(result)
+
+    asyncio.run(main())
